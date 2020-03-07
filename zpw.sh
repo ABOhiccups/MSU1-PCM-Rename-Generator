c0a25d6fac841afb9f26e7926c6596470bc1a1cb
ABOhiccups <abohiccups@gmail.com>
2020-03-07 16:16:13 -0500

    Add files via upload

---
+++ zpw.sh
@@ -0,0 +1,70 @@
+mv alttp_msu.msu zpw.msu
+mv alttp_msu-1.pcm zpw-1.pcm
+mv alttp_msu-2.pcm zpw-2.pcm
+mv alttp_msu-3.pcm zpw-3.pcm
+mv alttp_msu-4.pcm zpw-4.pcm
+mv alttp_msu-5.pcm zpw-5.pcm
+mv alttp_msu-6.pcm zpw-6.pcm
+mv alttp_msu-7.pcm zpw-7.pcm
+mv alttp_msu-8.pcm zpw-8.pcm
+mv alttp_msu-9.pcm zpw-9.pcm
+mv alttp_msu-10.pcm zpw-10.pcm
+mv alttp_msu-11.pcm zpw-11.pcm
+mv alttp_msu-12.pcm zpw-12.pcm
+mv alttp_msu-13.pcm zpw-13.pcm
+mv alttp_msu-14.pcm zpw-14.pcm
+mv alttp_msu-16.pcm zpw-16.pcm
+mv alttp_msu-17.pcm zpw-17.pcm
+mv alttp_msu-18.pcm zpw-18.pcm
+mv alttp_msu-19.pcm zpw-19.pcm
+mv alttp_msu-20.pcm zpw-20.pcm
+mv alttp_msu-21.pcm zpw-21.pcm
+mv alttp_msu-22.pcm zpw-22.pcm
+mv alttp_msu-23.pcm zpw-23.pcm
+mv alttp_msu-24.pcm zpw-24.pcm
+mv alttp_msu-25.pcm zpw-25.pcm
+mv alttp_msu-26.pcm zpw-26.pcm
+mv alttp_msu-27.pcm zpw-27.pcm
+mv alttp_msu-28.pcm zpw-28.pcm
+mv alttp_msu-29.pcm zpw-29.pcm
+mv alttp_msu-30.pcm zpw-30.pcm
+mv alttp_msu-31.pcm zpw-31.pcm
+mv alttp_msu-32.pcm zpw-32.pcm
+mv alttp_msu-33.pcm zpw-33.pcm
+mv alttp_msu-34.pcm zpw-34.pcm
+cp zpw-1.pcm zpw-15.pcm
+mv zpw.msu "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-1.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-2.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-3.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-4.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-5.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-6.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-7.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-8.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-9.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-10.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-11.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-12.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-13.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-14.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-15.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-16.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-17.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-18.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-19.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-20.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-21.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-22.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-23.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-24.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-25.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-26.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-27.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-28.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-29.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-30.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-31.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-32.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-33.pcm "The Legend of Zelda - Parallel Worlds MSU1"
+mv zpw-34.pcm "The Legend of Zelda - Parallel Worlds MSU1"
