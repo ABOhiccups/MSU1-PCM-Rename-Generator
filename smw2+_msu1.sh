4e74d8ef23b520b7d870bbdb7faa0bb8e164608a
ABOhiccups <abohiccups@gmail.com>
2020-03-07 23:49:49 -0500

    Add files via upload

---
+++ smw2+_msu1.sh
@@ -0,0 +1,60 @@
+mv smw2_msu1-2.pcm smw2+_msu1-2.pcm
+mv smw2_msu1-3.pcm smw2+_msu1-3.pcm
+mv smw2_msu1-4.pcm smw2+_msu1-4.pcm
+mv smw2_msu1-5.pcm smw2+_msu1-5.pcm
+mv smw2_msu1-6.pcm smw2+_msu1-6.pcm
+mv smw2_msu1-7.pcm smw2+_msu1-7.pcm
+mv smw2_msu1-8.pcm smw2+_msu1-8.pcm
+mv smw2_msu1-10.pcm smw2+_msu1-10.pcm
+mv smw2_msu1-11.pcm smw2+_msu1-11.pcm
+mv smw2_msu1-12.pcm smw2+_msu1-12.pcm
+mv smw2_msu1-13.pcm smw2+_msu1-13.pcm
+mv smw2_msu1-14.pcm smw2+_msu1-14.pcm
+mv smw2_msu1-15.pcm smw2+_msu1-15.pcm
+mv smw2_msu1-16.pcm smw2+_msu1-16.pcm
+mv smw2_msu1-17.pcm smw2+_msu1-17.pcm
+mv smw2_msu1-18.pcm smw2+_msu1-18.pcm
+mv smw2_msu1-19.pcm smw2+_msu1-19.pcm
+mv smw2_msu1-20.pcm smw2+_msu1-20.pcm
+mv smw2_msu1-22.pcm smw2+_msu1-22.pcm
+mv smw2_msu1-23.pcm smw2+_msu1-23.pcm
+mv smw2_msu1-24.pcm smw2+_msu1-24.pcm
+mv smw2_msu1-25.pcm smw2+_msu1-25.pcm
+mv smw2_msu1-26.pcm smw2+_msu1-26.pcm
+mv smw2_msu1-28.pcm smw2+_msu1-28.pcm
+mv smw2_msu1-32.pcm smw2+_msu1-32.pcm
+mv smw2_msu1-33.pcm smw2+_msu1-33.pcm
+mv smw2_msu1-34.pcm smw2+_msu1-34.pcm
+mv smw2_msu1-35.pcm smw2+_msu1-35.pcm
+mv smw2_msu1-36.pcm smw2+_msu1-36.pcm
+mv smw2_msu1-37.pcm smw2+_msu1-37.pcm
+mv smw2+_msu1-2.pcm "SMW2+ MSU1"
+mv smw2+_msu1-3.pcm "SMW2+ MSU1"
+mv smw2+_msu1-4.pcm "SMW2+ MSU1"
+mv smw2+_msu1-5.pcm "SMW2+ MSU1"
+mv smw2+_msu1-6.pcm "SMW2+ MSU1"
+mv smw2+_msu1-7.pcm "SMW2+ MSU1"
+mv smw2+_msu1-8.pcm "SMW2+ MSU1"
+mv smw2+_msu1-10.pcm "SMW2+ MSU1"
+mv smw2+_msu1-11.pcm "SMW2+ MSU1"
+mv smw2+_msu1-12.pcm "SMW2+ MSU1"
+mv smw2+_msu1-13.pcm "SMW2+ MSU1"
+mv smw2+_msu1-14.pcm "SMW2+ MSU1"
+mv smw2+_msu1-15.pcm "SMW2+ MSU1"
+mv smw2+_msu1-16.pcm "SMW2+ MSU1"
+mv smw2+_msu1-17.pcm "SMW2+ MSU1"
+mv smw2+_msu1-18.pcm "SMW2+ MSU1"
+mv smw2+_msu1-19.pcm "SMW2+ MSU1"
+mv smw2+_msu1-20.pcm "SMW2+ MSU1"
+mv smw2+_msu1-22.pcm "SMW2+ MSU1"
+mv smw2+_msu1-23.pcm "SMW2+ MSU1"
+mv smw2+_msu1-24.pcm "SMW2+ MSU1"
+mv smw2+_msu1-25.pcm "SMW2+ MSU1"
+mv smw2+_msu1-26.pcm "SMW2+ MSU1"
+mv smw2+_msu1-28.pcm "SMW2+ MSU1"
+mv smw2+_msu1-32.pcm "SMW2+ MSU1"
+mv smw2+_msu1-33.pcm "SMW2+ MSU1"
+mv smw2+_msu1-34.pcm "SMW2+ MSU1"
+mv smw2+_msu1-35.pcm "SMW2+ MSU1"
+mv smw2+_msu1-36.pcm "SMW2+ MSU1"
+mv smw2+_msu1-37.pcm "SMW2+ MSU1"
