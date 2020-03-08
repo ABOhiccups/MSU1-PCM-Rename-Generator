79a7d51559ed73bf2c9e4aa83474f7cdd90cd161
ABOhiccups <abohiccups@gmail.com>
2020-03-08 00:15:33 -0500

    Add files via upload

---
+++ smw2+2_msu1.bat
@@ -0,0 +1,61 @@
+@echo off
+ren smw2_msu1-2.pcm smw2+2_msu1-2.pcm
+ren smw2_msu1-3.pcm smw2+2_msu1-3.pcm
+ren smw2_msu1-4.pcm smw2+2_msu1-4.pcm
+ren smw2_msu1-5.pcm smw2+2_msu1-5.pcm
+ren smw2_msu1-6.pcm smw2+2_msu1-6.pcm
+ren smw2_msu1-7.pcm smw2+2_msu1-7.pcm
+ren smw2_msu1-8.pcm smw2+2_msu1-8.pcm
+ren smw2_msu1-10.pcm smw2+2_msu1-10.pcm
+ren smw2_msu1-11.pcm smw2+2_msu1-11.pcm
+ren smw2_msu1-12.pcm smw2+2_msu1-12.pcm
+ren smw2_msu1-13.pcm smw2+2_msu1-13.pcm
+ren smw2_msu1-14.pcm smw2+2_msu1-14.pcm
+ren smw2_msu1-15.pcm smw2+2_msu1-15.pcm
+ren smw2_msu1-16.pcm smw2+2_msu1-16.pcm
+ren smw2_msu1-17.pcm smw2+2_msu1-17.pcm
+ren smw2_msu1-18.pcm smw2+2_msu1-18.pcm
+ren smw2_msu1-19.pcm smw2+2_msu1-19.pcm
+ren smw2_msu1-20.pcm smw2+2_msu1-20.pcm
+ren smw2_msu1-22.pcm smw2+2_msu1-22.pcm
+ren smw2_msu1-23.pcm smw2+2_msu1-23.pcm
+ren smw2_msu1-24.pcm smw2+2_msu1-24.pcm
+ren smw2_msu1-25.pcm smw2+2_msu1-25.pcm
+ren smw2_msu1-26.pcm smw2+2_msu1-26.pcm
+ren smw2_msu1-28.pcm smw2+2_msu1-28.pcm
+ren smw2_msu1-32.pcm smw2+2_msu1-32.pcm
+ren smw2_msu1-33.pcm smw2+2_msu1-33.pcm
+ren smw2_msu1-34.pcm smw2+2_msu1-34.pcm
+ren smw2_msu1-35.pcm smw2+2_msu1-35.pcm
+ren smw2_msu1-36.pcm smw2+2_msu1-36.pcm
+ren smw2_msu1-37.pcm smw2+2_msu1-37.pcm
+move smw2+2_msu1-2.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-3.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-4.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-5.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-6.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-7.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-8.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-10.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-11.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-12.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-13.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-14.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-15.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-16.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-17.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-18.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-19.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-20.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-22.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-23.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-24.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-25.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-26.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-28.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-32.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-33.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-34.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-35.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-36.pcm "SMW2+2 MSU1"
+move smw2+2_msu1-37.pcm "SMW2+2 MSU1"
