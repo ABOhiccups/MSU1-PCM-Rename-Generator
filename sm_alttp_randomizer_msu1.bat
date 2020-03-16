a654faa67c79d704601f2fb91d55ee1a6a220e56
ABOhiccups <abohiccups@gmail.com>
2020-03-16 15:02:26 -0400

    Add files via upload

---
+++ sm_alttp_randomizer_msu1.bat
@@ -0,0 +1,123 @@
+@echo off
+ren supermetroid_msu1-1.pcm sm_alttp_randomizer_msu1-1.pcm
+ren supermetroid_msu1-2.pcm sm_alttp_randomizer_msu1-2.pcm
+ren supermetroid_msu1-3.pcm sm_alttp_randomizer_msu1-3.pcm
+ren supermetroid_msu1-4.pcm sm_alttp_randomizer_msu1-4.pcm
+ren supermetroid_msu1-5.pcm sm_alttp_randomizer_msu1-5.pcm
+ren supermetroid_msu1-6.pcm sm_alttp_randomizer_msu1-6.pcm
+ren supermetroid_msu1-7.pcm sm_alttp_randomizer_msu1-7.pcm
+ren supermetroid_msu1-8.pcm sm_alttp_randomizer_msu1-8.pcm
+ren supermetroid_msu1-9.pcm sm_alttp_randomizer_msu1-9.pcm
+ren supermetroid_msu1-10.pcm sm_alttp_randomizer_msu1-10.pcm
+ren supermetroid_msu1-11.pcm sm_alttp_randomizer_msu1-11.pcm
+ren supermetroid_msu1-12.pcm sm_alttp_randomizer_msu1-12.pcm
+ren supermetroid_msu1-13.pcm sm_alttp_randomizer_msu1-13.pcm
+ren supermetroid_msu1-14.pcm sm_alttp_randomizer_msu1-14.pcm
+ren supermetroid_msu1-15.pcm sm_alttp_randomizer_msu1-15.pcm
+ren supermetroid_msu1-16.pcm sm_alttp_randomizer_msu1-16.pcm
+ren supermetroid_msu1-17.pcm sm_alttp_randomizer_msu1-17.pcm
+ren supermetroid_msu1-18.pcm sm_alttp_randomizer_msu1-18.pcm
+ren supermetroid_msu1-19.pcm sm_alttp_randomizer_msu1-19.pcm
+ren supermetroid_msu1-20.pcm sm_alttp_randomizer_msu1-20.pcm
+ren supermetroid_msu1-21.pcm sm_alttp_randomizer_msu1-21.pcm
+ren supermetroid_msu1-22.pcm sm_alttp_randomizer_msu1-22.pcm
+ren supermetroid_msu1-23.pcm sm_alttp_randomizer_msu1-23.pcm
+ren supermetroid_msu1-24.pcm sm_alttp_randomizer_msu1-24.pcm
+ren supermetroid_msu1-25.pcm sm_alttp_randomizer_msu1-25.pcm
+ren supermetroid_msu1-26.pcm sm_alttp_randomizer_msu1-26.pcm
+ren supermetroid_msu1-27.pcm sm_alttp_randomizer_msu1-27.pcm
+ren supermetroid_msu1-28.pcm sm_alttp_randomizer_msu1-28.pcm
+ren supermetroid_msu1-29.pcm sm_alttp_randomizer_msu1-29.pcm
+ren alttp_msu-1.pcm sm_alttp_randomizer_msu1-101.pcm
+ren alttp_msu-2.pcm sm_alttp_randomizer_msu1-102.pcm
+ren alttp_msu-3.pcm sm_alttp_randomizer_msu1-103.pcm
+ren alttp_msu-4.pcm sm_alttp_randomizer_msu1-104.pcm
+ren alttp_msu-5.pcm sm_alttp_randomizer_msu1-105.pcm
+ren alttp_msu-6.pcm sm_alttp_randomizer_msu1-106.pcm
+ren alttp_msu-7.pcm sm_alttp_randomizer_msu1-107.pcm
+ren alttp_msu-8.pcm sm_alttp_randomizer_msu1-108.pcm
+ren alttp_msu-9.pcm sm_alttp_randomizer_msu1-109.pcm
+ren alttp_msu-10.pcm sm_alttp_randomizer_msu1-110.pcm
+ren alttp_msu-11.pcm sm_alttp_randomizer_msu1-111.pcm
+ren alttp_msu-12.pcm sm_alttp_randomizer_msu1-112.pcm
+ren alttp_msu-13.pcm sm_alttp_randomizer_msu1-113.pcm
+ren alttp_msu-14.pcm sm_alttp_randomizer_msu1-114.pcm
+ren alttp_msu-16.pcm sm_alttp_randomizer_msu1-116.pcm
+ren alttp_msu-17.pcm sm_alttp_randomizer_msu1-117.pcm
+ren alttp_msu-18.pcm sm_alttp_randomizer_msu1-118.pcm
+ren alttp_msu-19.pcm sm_alttp_randomizer_msu1-119.pcm
+ren alttp_msu-20.pcm sm_alttp_randomizer_msu1-120.pcm
+ren alttp_msu-21.pcm sm_alttp_randomizer_msu1-121.pcm
+ren alttp_msu-22.pcm sm_alttp_randomizer_msu1-122.pcm
+ren alttp_msu-23.pcm sm_alttp_randomizer_msu1-123.pcm
+ren alttp_msu-24.pcm sm_alttp_randomizer_msu1-124.pcm
+ren alttp_msu-25.pcm sm_alttp_randomizer_msu1-125.pcm
+ren alttp_msu-26.pcm sm_alttp_randomizer_msu1-126.pcm
+ren alttp_msu-27.pcm sm_alttp_randomizer_msu1-127.pcm
+ren alttp_msu-28.pcm sm_alttp_randomizer_msu1-128.pcm
+ren alttp_msu-29.pcm sm_alttp_randomizer_msu1-129.pcm
+ren alttp_msu-30.pcm sm_alttp_randomizer_msu1-130.pcm
+ren alttp_msu-31.pcm sm_alttp_randomizer_msu1-131.pcm
+ren alttp_msu-32.pcm sm_alttp_randomizer_msu1-132.pcm
+ren alttp_msu-33.pcm sm_alttp_randomizer_msu1-133.pcm
+move sm_alttp_randomizer_msu1-1.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-2.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-3.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-4.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-5.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-6.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-7.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-8.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-9.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-10.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-11.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-12.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-13.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-14.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-15.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-16.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-17.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-18.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-19.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-20.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-21.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-22.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-23.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-24.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-25.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-26.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-27.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-28.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-29.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-101.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-102.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-103.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-104.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-105.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-106.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-107.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-108.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-109.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-110.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-111.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-112.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-113.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-114.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-116.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-117.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-118.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-119.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-120.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-121.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-122.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-123.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-124.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-125.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-126.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-127.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-128.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-129.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-130.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-131.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-132.pcm "SM and ALTTP Randomizer MSU1"
+move sm_alttp_randomizer_msu1-133.pcm "SM and ALTTP Randomizer MSU1"
---
+++ sm_alttp_randomizer_msu1.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+mv supermetroid_msu1-1.pcm sm_alttp_randomizer_msu1-1.pcm
+mv supermetroid_msu1-2.pcm sm_alttp_randomizer_msu1-2.pcm
+mv supermetroid_msu1-3.pcm sm_alttp_randomizer_msu1-3.pcm
+mv supermetroid_msu1-4.pcm sm_alttp_randomizer_msu1-4.pcm
+mv supermetroid_msu1-5.pcm sm_alttp_randomizer_msu1-5.pcm
+mv supermetroid_msu1-6.pcm sm_alttp_randomizer_msu1-6.pcm
+mv supermetroid_msu1-7.pcm sm_alttp_randomizer_msu1-7.pcm
+mv supermetroid_msu1-8.pcm sm_alttp_randomizer_msu1-8.pcm
+mv supermetroid_msu1-9.pcm sm_alttp_randomizer_msu1-9.pcm
+mv supermetroid_msu1-10.pcm sm_alttp_randomizer_msu1-10.pcm
+mv supermetroid_msu1-11.pcm sm_alttp_randomizer_msu1-11.pcm
+mv supermetroid_msu1-12.pcm sm_alttp_randomizer_msu1-12.pcm
+mv supermetroid_msu1-13.pcm sm_alttp_randomizer_msu1-13.pcm
+mv supermetroid_msu1-14.pcm sm_alttp_randomizer_msu1-14.pcm
+mv supermetroid_msu1-15.pcm sm_alttp_randomizer_msu1-15.pcm
+mv supermetroid_msu1-16.pcm sm_alttp_randomizer_msu1-16.pcm
+mv supermetroid_msu1-17.pcm sm_alttp_randomizer_msu1-17.pcm
+mv supermetroid_msu1-18.pcm sm_alttp_randomizer_msu1-18.pcm
+mv supermetroid_msu1-19.pcm sm_alttp_randomizer_msu1-19.pcm
+mv supermetroid_msu1-20.pcm sm_alttp_randomizer_msu1-20.pcm
+mv supermetroid_msu1-21.pcm sm_alttp_randomizer_msu1-21.pcm
+mv supermetroid_msu1-22.pcm sm_alttp_randomizer_msu1-22.pcm
+mv supermetroid_msu1-23.pcm sm_alttp_randomizer_msu1-23.pcm
+mv supermetroid_msu1-24.pcm sm_alttp_randomizer_msu1-24.pcm
+mv supermetroid_msu1-25.pcm sm_alttp_randomizer_msu1-25.pcm
+mv supermetroid_msu1-26.pcm sm_alttp_randomizer_msu1-26.pcm
+mv supermetroid_msu1-27.pcm sm_alttp_randomizer_msu1-27.pcm
+mv supermetroid_msu1-28.pcm sm_alttp_randomizer_msu1-28.pcm
+mv supermetroid_msu1-29.pcm sm_alttp_randomizer_msu1-29.pcm
+mv alttp_msu-1.pcm sm_alttp_randomizer_msu1-101.pcm
+mv alttp_msu-2.pcm sm_alttp_randomizer_msu1-102.pcm
+mv alttp_msu-3.pcm sm_alttp_randomizer_msu1-103.pcm
+mv alttp_msu-4.pcm sm_alttp_randomizer_msu1-104.pcm
+mv alttp_msu-5.pcm sm_alttp_randomizer_msu1-105.pcm
+mv alttp_msu-6.pcm sm_alttp_randomizer_msu1-106.pcm
+mv alttp_msu-7.pcm sm_alttp_randomizer_msu1-107.pcm
+mv alttp_msu-8.pcm sm_alttp_randomizer_msu1-108.pcm
+mv alttp_msu-9.pcm sm_alttp_randomizer_msu1-109.pcm
+mv alttp_msu-10.pcm sm_alttp_randomizer_msu1-110.pcm
+mv alttp_msu-11.pcm sm_alttp_randomizer_msu1-111.pcm
+mv alttp_msu-12.pcm sm_alttp_randomizer_msu1-112.pcm
+mv alttp_msu-13.pcm sm_alttp_randomizer_msu1-113.pcm
+mv alttp_msu-14.pcm sm_alttp_randomizer_msu1-114.pcm
+mv alttp_msu-16.pcm sm_alttp_randomizer_msu1-116.pcm
+mv alttp_msu-17.pcm sm_alttp_randomizer_msu1-117.pcm
+mv alttp_msu-18.pcm sm_alttp_randomizer_msu1-118.pcm
+mv alttp_msu-19.pcm sm_alttp_randomizer_msu1-119.pcm
+mv alttp_msu-20.pcm sm_alttp_randomizer_msu1-120.pcm
+mv alttp_msu-21.pcm sm_alttp_randomizer_msu1-121.pcm
+mv alttp_msu-22.pcm sm_alttp_randomizer_msu1-122.pcm
+mv alttp_msu-23.pcm sm_alttp_randomizer_msu1-123.pcm
+mv alttp_msu-24.pcm sm_alttp_randomizer_msu1-124.pcm
+mv alttp_msu-25.pcm sm_alttp_randomizer_msu1-125.pcm
+mv alttp_msu-26.pcm sm_alttp_randomizer_msu1-126.pcm
+mv alttp_msu-27.pcm sm_alttp_randomizer_msu1-127.pcm
+mv alttp_msu-28.pcm sm_alttp_randomizer_msu1-128.pcm
+mv alttp_msu-29.pcm sm_alttp_randomizer_msu1-129.pcm
+mv alttp_msu-30.pcm sm_alttp_randomizer_msu1-130.pcm
+mv alttp_msu-31.pcm sm_alttp_randomizer_msu1-131.pcm
+mv alttp_msu-32.pcm sm_alttp_randomizer_msu1-132.pcm
+mv alttp_msu-33.pcm sm_alttp_randomizer_msu1-133.pcm
+mv sm_alttp_randomizer_msu1-1.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-2.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-3.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-4.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-5.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-6.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-7.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-8.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-9.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-10.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-11.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-12.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-13.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-14.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-15.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-16.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-17.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-18.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-19.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-20.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-21.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-22.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-23.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-24.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-25.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-26.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-27.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-28.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-29.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-101.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-102.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-103.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-104.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-105.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-106.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-107.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-108.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-109.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-110.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-111.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-112.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-113.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-114.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-116.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-117.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-118.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-119.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-120.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-121.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-122.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-123.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-124.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-125.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-126.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-127.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-128.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-129.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-130.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-131.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-132.pcm "SM and ALTTP Randomizer MSU1"
+mv sm_alttp_randomizer_msu1-133.pcm "SM and ALTTP Randomizer MSU1"
